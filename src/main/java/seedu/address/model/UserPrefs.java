7c00065d0c331b8954716d2fe86f62b6b81a7c2d
nhocmt227 <phong1572005@gmail.com>
2025-03-13 23:59:22 +0800

    Change json file name

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path medLoggerFilePath = Paths.get("data" , "addressbook.json");
+    private Path medLoggerFilePath = Paths.get("data" , "MedLogger.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
