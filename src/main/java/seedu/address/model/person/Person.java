013a68a3ac150892a4439cc396ce2e642c71f910
Deng Tianle <dengtianle51@gmail.com>
2025-03-21 00:40:57 +0800

    Fix bugs
    Fix mismatch between java and fxml. Also ensure require all non-null in
    Person.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -32,7 +32,7 @@ public class Person {
      */
     public Person(Name name, Phone phone, Email email, Address address,
                   DateTime dateTime, Remark remark, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address, dateTime, remark, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
