62c6d9a2be7fd2ec86b804da02f0cb0050af4486
Taoseeker <e1155335@u.nus.edu>
2025-03-26 23:42:59 +0800

    Add new class Visit

--- src/main/java/seedu/address/model/person/Visit.java
+++ src/main/java/seedu/address/model/person/Visit.java
@@ -1,5 +1,47 @@
 package seedu.address.model.person;
+import seedu.address.commons.util.ToStringBuilder;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 public class Visit {
+    public final Person person ;
+    public final DateTime dateTime;
+    public final Remark remark;
 
+    Visit(Person person, DateTime dateTime, Remark remark) {
+        requireAllNonNull(person, dateTime, remark);
+        this.person = person;
+        this.dateTime = dateTime;
+        this.remark = remark;
+    }
+
+    public Person getPerson() {
+        return this.person;
+    }
+
+    public Remark getRemark() {
+        return this.remark;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        } else if (object instanceof Visit other) {
+            return other.person.equals(this.person)
+                    && other.dateTime.equals(this.dateTime)
+                    && other.remark.equals(this.remark);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("person", person)
+                .add("visit time", dateTime)
+                .add("remark", remark)
+                .toString();
+    }
 }
