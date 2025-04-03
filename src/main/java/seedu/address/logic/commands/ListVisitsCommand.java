c65dce5ae9b091545b9b3364aa5242904ed35c71
Qiming <e0271228@u.nus.edu>
2025-04-03 22:26:43 +0800

    Implement listvisits command

--- src/main/java/seedu/address/logic/commands/ListVisitsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListVisitsCommand.java
@@ -4,9 +4,12 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.Model;
 
+/**
+ * Lists all visits in the visit list panel.
+ */
 public class ListVisitsCommand extends Command {
 
-    public static final String COMMAND_WORD = "listVisits";
+    public static final String COMMAND_WORD = "listvisits";
     public static final String MESSAGE_SUCCESS = "Listed all visits.";
 
     @Override
--- src/main/java/seedu/address/logic/parser/ListVisitsCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListVisitsCommandParser.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import seedu.address.logic.commands.ListVisitsCommand;
 
+/**
+ * Parses input arguments and creates a new ListVisitsCommand object.
+ */
 public class ListVisitsCommandParser implements Parser<ListVisitsCommand> {
     @Override
     public ListVisitsCommand parse(String args) {
