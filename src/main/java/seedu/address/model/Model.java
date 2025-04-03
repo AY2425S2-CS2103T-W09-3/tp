e08c69d200b669afdc826aa710be73c6887e8720
Deng Tianle <dengtianle51@gmail.com>
2025-04-04 00:20:53 +0800

    Fix bugs arising from merge

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -6,7 +6,6 @@ import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Visit;
@@ -73,13 +72,15 @@ public interface Model {
      */
     boolean hasVisit(Visit visit);
 
-    void addVisit(Visit visit) throws CommandException;
+    void addVisit(Visit visit);
 
     /**
      * Deletes the given visit.
      * The visit must exist in the Medlogger
      */
     void deleteVisit(Visit target);
+
+    void setVisit(Visit target, Visit editedVisit);
 
     /**
      * Deletes the given person.
