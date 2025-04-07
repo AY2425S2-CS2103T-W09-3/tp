bb10c8db7b2289e4fe581582792078c953077f3f
Qiming <e0271228@u.nus.edu>
2025-04-07 23:17:18 +0800

    Handle invalid sg phone number

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -12,8 +12,8 @@ public class Phone {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
-    public static final Phone DUMMY_PHONE = new Phone("111111111111111111111");
+    public static final String VALIDATION_REGEX = "^[689]\\d{7}$";;
+    public static final Phone DUMMY_PHONE = new Phone("98765432");
     public final String value;
 
     /**
