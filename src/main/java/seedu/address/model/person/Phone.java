4833d19490ed05210cf7e4e7b5f0ce27763f4d1e
Qiming <e0271228@u.nus.edu>
2025-04-08 00:03:00 +0800

    Update testcases to pass 76 cases

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -12,7 +12,7 @@ public class Phone {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Singapore Phone numbers should contain eight digits with first digit being 6, 8 or 9. ";
-    public static final String VALIDATION_REGEX = "^[689]\\d{7}$";;
+    public static final String VALIDATION_REGEX = "^[689]\\d{7}$";
     public static final Phone DUMMY_PHONE = new Phone("98765432");
     public final String value;
 
