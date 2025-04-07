ce9e8035ba4bbdd50f6364e4243004415abcfc88
Qiming <e0271228@u.nus.edu>
2025-04-07 22:38:09 +0800

    Fix format error

--- src/main/java/seedu/address/logic/commands/ListVisitsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListVisitsCommand.java
@@ -20,9 +20,9 @@ public class ListVisitsCommand extends Command {
     public static final String COMMAND_WORD = "listvisits";
     public static final String MESSAGE_SUCCESS = "Listed all visits";
     public static final String MESSAGE_USAGE = "listvisits: Lists visits with optional filters.\n"
-    + "Parameters: [i/NRIC] [l/LIMIT] [from/DATE] [to/DATE] [today/] "
-    + "[sym/SYMPTOM] [diag/DIAGNOSIS] [med/MEDICATION]\n"
-    + "Example: listvisits i/S1234567A from/2023-01-01 to/2023-12-31 diagnosis/flu";
+            + "Parameters: [i/NRIC] [l/LIMIT] [from/DATE] [to/DATE] [today/] "
+            + "[sym/SYMPTOM] [diag/DIAGNOSIS] [med/MEDICATION]\n"
+            + "Example: listvisits i/S1234567A from/2023-01-01 to/2023-12-31 diagnosis/flu";
 
     private final Nric nric;
     private final Integer limit;
